6a822185c2854e83d0e8c80da593fc714eaa6e47
Vinícius Vezono <vinicius.vezono123@gmail.com>
2025-04-13 22:22:23 -0300

    Tela config login com User

--- app-crud/resources/views/users.blade.php
+++ app-crud/resources/views/users.blade.php
@@ -2,18 +2,16 @@
 
 @section('content')
 
-    <h2 class="text-2xl font-semibold mb-4">Usuários</h2>
+    <h2 class="text-2xl font-semibold mb-4">Listas de usuarios</h2>
 
     <a href="{{ route('users.create')}}"> Criar usuarios </a>
     
-    <!-- Tabela -->
     <table class="min-w-full table-auto border-separate border-spacing-2 border border-gray-400 dark:border-gray-500 rounded-lg shadow-lg">
         <thead>
             <tr class="bg-gray-100 dark:bg-gray-700">
                 <th class="px-4 py-2 text-left text-sm font-medium text-gray-700 dark:text-gray-300 border border-gray-300 dark:border-gray-600">Nome</th>
                 <th class="px-4 py-2 text-left text-sm font-medium text-gray-700 dark:text-gray-300 border border-gray-300 dark:border-gray-600">Senha</th>
-                <th class="px-4 py-2 text-left text-sm font-medium text-gray-700 dark:text-gray-300 border border-gray-300 dark:border-gray-600">Edit</th>
-                <th class="px-4 py-2 text-left text-sm font-medium text-gray-700 dark:text-gray-300 border border-gray-300 dark:border-gray-600">Detalhar usuario</th>
+                <th class="px-4 py-2 text-left text-sm font-medium text-gray-700 dark:text-gray-300 border border-gray-300 dark:border-gray-600"></th>
             </tr>
         </thead>
         <tbody class="bg-white dark:bg-gray-800">
@@ -22,7 +20,7 @@
                 <td class="px-4 py-2 text-sm text-gray-700 dark:text-gray-300 border border-gray-300 dark:border-gray-700">{{ $user->name }}</td>
                 <td class="px-4 py-2 text-sm text-gray-700 dark:text-gray-300 border border-gray-300 dark:border-gray-700">{{ $user->password }}</td>
                 <td class="px-4 py-2 text-sm text-green-700 dark:text-gray"><a href="{{ route('users.edit',['user' => $user -> id]) }}">Edit </a></td>
-                <td class="px-4 py-2 text-sm text-green-700 "><a href="{{ route('users.show', ['user' => $user->id]) }}">Compras</a></td>
+                <td class="px-4 py-2 text-sm text-green-700 "><a href="{{ route('users.show', ['user' => $user->id]) }}">Excluir</a></td>
             </tr>
             @endforeach
             <tr class="hover:bg-gray-50 dark:hover:bg-gray-600 transition-colors duration-200">
--- app-crud/routes/web.php
+++ app-crud/routes/web.php
@@ -1,19 +1,21 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use  App\Http\Controllers\UserController;
-
-Route::get('/', [HomeController::class, 'index']) ->name('home');
+use App\Http\Controllers\UserController;
 
 
-// Route::get('/users', [UserController::class, 'index']) ->name('users.index');
-// Route::get('/users/create', [UserController::class, 'create']) ->name('users.create'); //rota para a criação do formulario
-// Route::post('/users', [UserController::class, 'store']) ->name('users.store');//rota para enviar os dados 
-// Route::get('/users/{user}', [UserController::class, 'show']) ->name('users.show'); //rota retornar um usuario especifico
-// Route::get('/users/{user}/edit', [UserController::class, 'edit']) ->name('users.edit'); //rota para formulario editar o usuario
-// Route::put('/users/{user}', [UserController::class, 'update']) ->name('users.update'); //rota para enviar as ediçoes
-// Route::delete('/users/{user}', [UserController::class, 'destroy']) ->name('users.destroy'); //rota para a 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
 Route::resource('users', UserController::class);
 
-
+require __DIR__.'/auth.php';
