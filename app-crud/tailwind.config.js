809ff79e322162e81b9a545dbba3fa8531c539dd
Vinícius Vezono <vinicius.vezono123@gmail.com>
2025-04-15 12:30:07 -0300

    final project

--- app-crud/tailwind.config.js
+++ app-crud/tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-      './resources/**/*.blade.php',     // Todos os arquivos Blade
-      './resources/**/*.js',            // Arquivos JS
-      './resources/**/*.vue',           // Se estiver usando Vue
-      './storage/framework/views/*.php' // Views compiladas
+      './resources/**/*.blade.php',     
+      './resources/**/*.js',            
+      './resources/**/*.vue',           
+      './storage/framework/views/*.php' 
     ],
     theme: {
       extend: {},
